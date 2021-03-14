4068dd93754f14f9fd7471bf37179fc5cf77a581
fabio <fabio@DESKTOP-U0UK4ER>
2021-03-14 01:09:35 +0100

    Soluzione 3

--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -9,15 +9,20 @@ public class TestLibretto {
 		System.out.println("Test metodi di Libretto");
 		Libretto libretto = new Libretto();
 		Voto voto1 = new Voto("Analisi 1", 30, LocalDate.of(2019, 2, 15));
+		// punto 1
 		libretto.add(voto1);
 		libretto.add(new Voto("Chimica", 25,LocalDate.of(2019, 9, 15)));
 		libretto.add(new Voto("Fisica 1", 28,LocalDate.of(2019, 7, 15)));
 		libretto.add(new Voto("Informatica", 24,LocalDate.of(2019, 9, 15)));
 		System.out.println(libretto);
+		// punto 2
 		List<Voto> listaVenticinque = libretto.listaVotiUguali(25);
 		System.out.println(listaVenticinque);
+		// punto 2 bis
 		Libretto venticinque = libretto.votiUguali(25);
 		System.out.println(venticinque);
+		// punto 3
+		System.out.println(libretto.ricercaCorso("Fisica 1"));
 	}
 
 }
